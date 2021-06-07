38b2334694339b5167d1bc190b270263460b58ee
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 20:37:07 -0400

    Solution to exercise 10

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,6 +3,10 @@
  *  Copyright 2021 Soroush Alinaghian
  */
 package base;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /*
 Create a simple self-checkout system. Prompt for the prices and quantities of three items. Calculate the subtotal of the items.
 Then calculate the tax using a tax rate of 5.5%. Print out the line items with the quantity and total, and then print out the subtotal, tax amount, and total.
@@ -31,7 +35,64 @@ Revise the program to ensure that prices and quantities are entered as numeric v
 Alter the program so that an indeterminate number of items can be entered. The tax and total are computed when there are no more items to be entered.
  */
 public class App {
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        int item1Price = readPriceOfItem1();
+        int item1Amount = readAmountOfItem1();
+        int item2Price = readPriceOfItem2();
+        int item2Amount = readAmountOfItem2();
+        int item3Price = readPriceOfItem3();
+        int item3Amount = readAmountOfItem3();
+        double subtotal = readSubtotal(item1Price, item1Amount, item2Price, item2Amount, item3Price, item3Amount);
+        float tax = readTax(subtotal);
+        generateTotal(subtotal, tax);
+    }
+
+    private static void generateTotal(double subtotal, float tax) {
+        double total = subtotal + tax;
+        System.out.println(String.format("Total: $%.2f", total));
+    }
+
+    private static float readTax(double subtotal) {
+        float taxRate = (float) (subtotal * .055);
+        System.out.println(String.format("Tax: $%.2f", taxRate));
+        return taxRate;
+    }
+
+    private static double readSubtotal(int item1Price, int item1Amount, int item2Price, int item2Amount, int item3Price, int item3Amount) {
+        double total =  (item1Price * item1Amount) + (item2Price * item2Amount) + (item3Price * item3Amount);
+        System.out.println(String.format("Subtotal: $%.2f", total));
+        return total;
+    }
+
+    private static int readPriceOfItem1() {
+        System.out.print("Enter the price of item 1: ");
+        return in.nextInt();
+    }
+
+    private static int readAmountOfItem1() {
+        System.out.print("Enter the quantity of item 1: ");
+        return in.nextInt();
+    }
+
+    private static int readPriceOfItem2() {
+        System.out.print("Enter the price of item 2: ");
+        return in.nextInt();
+    }
+
+    private static int readAmountOfItem2() {
+        System.out.print("Enter the quantity of item 2: ");
+        return in.nextInt();
+    }
+
+    private static int readPriceOfItem3() {
+        System.out.print("Enter the price of item 3: ");
+        return in.nextInt();
+    }
+
+    private static int readAmountOfItem3() {
+        System.out.print("Enter the quantity of item 3: ");
+        return in.nextInt();
     }
 }
